8173365daa088686b6f8ebfb260ed1a97b40e857
Ben Halverson <benhalverson33@gmail.com>
2021-05-03 00:05:39 -0700

    added error handling and refactor util methods to new file

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import * as cors from 'cors';
-import axios from 'axios';
 import { FireHolFile } from './interfaces';
-import geoip from 'geoip-lite';
+import { getData, getIPLocationInfo, readData } from './utils';
 
 //module version is not available for this package
 const validate = require('ip-validator');
@@ -28,70 +27,43 @@ app.use(helmet());
 app.get('/:ipAddress', async (req: Request, res: Response) => {
 	let { ipAddress } = req.params;
 
-	let lists: Array<FireHolFile> = await getData();
-	let flagged = false;
-	let count = 0;
-	let foundIn: string | null = null;
-	for (let i: number = 0, num: number = lists.length; i < num; ++i) {
-		let lines: string[] = await readData(lists[i]);
-		count = count + lines.length;
-		if (lines.includes(validate.ipv4(ipAddress))) {
-			flagged = true;
-			foundIn = `https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${lists[i].path}`;
-			break;
-		}
-	}
-	let telemetry = await getIPLocationInfo(ipAddress);
-	let message = `The ip address is ${ipAddress}`;
-	flagged ? (message += ` was found amoung an ipset.`) : (message += ` is ok.`);
+	try {
+		let lists: Array<FireHolFile> = await getData();
+		let flagged = false;
+		let count = 0;
+		let foundIn: string | null = null;
+		let telemetry = await getIPLocationInfo(ipAddress);
+		let message = `The ip address is ${ipAddress}`;
+		flagged ? (message += ` was found amoung an ipset.`) : (message += ` is ok.`);
 
-	res.json({
-		flagged: flagged,
-		message: message,
-		telemetry: telemetry,
-		ipset: foundIn,
-		'ipsets-count': lists.length
-	});
+		if (validate.ipv4(ipAddress)) {
+			for (let i: number = 0, num: number = lists.length; i < num; ++i) {
+				let lines: string[] = await readData(lists[i]);
+				count = count + lines.length;
+				if (lines.includes(ipAddress)) {
+					flagged = true;
+					foundIn = `https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${lists[i].path}`;
+					break;
+				}
+			}
+		} else {
+			throw new Error('Not a valid IP address.');
+		}
+
+		res.json({
+			origin: req.originalUrl,
+			flagged: flagged,
+			message: message,
+			telemetry: telemetry,
+			ipset: foundIn,
+			'ipsets-count': lists.length
+		});
+	} catch (error) {
+		res.json({
+			'Error': error.message
+		})	
+	}
 });
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
-
-const getData = async () => {
-	const response = await axios.get(
-		`https://api.github.com/repos/firehol/blocklist-ipsets/git/trees/master?recursive=1`
-	);
-
-	const files: [] = await response.data.tree.filter((file: any) => {
-		if (file.path.endsWith('.ipset')) {
-			return file.path;
-		}
-	});
-	return files;
-};
-
-const readData = async (file: FireHolFile) => {
-	const response = await axios.get(`https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${file.path}`);
-
-	//data here is the ip address in each file
-	const cleanedText: [] = await response.data.split('\n').filter((line: any) => {
-		return line.includes('#') !== 0;
-	});
-	return cleanedText;
-};
-
-// Determine location of IP Address.
-const getIPLocationInfo = async (ip: string) => {
-	let info = await geoip.lookup(ip);
-
-	if (info) {
-		return [
-			{
-				region: info.region,
-				country: info.country,
-				timezone: info.timezone
-			}
-		];
-	}
-	return 'no data found';
-};
---
+++ utils.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import geoip from 'geoip-lite';
+
+import { FireHolFile } from './interfaces';
+
+export const getData = async () => {
+	const response = await axios.get(
+		`https://api.github.com/repos/firehol/blocklist-ipsets/git/trees/master?recursive=1`
+	);
+
+  //files is each filename in the git repo that ends with .ipset
+	const files: [] = await response.data.tree.filter((file: any) => {
+		if (file.path.endsWith('.ipset')) {
+			return file.path;
+		}
+	});
+	return files;
+};
+
+
+export const readData = async (file: FireHolFile) => {
+	const response = await axios.get(`https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${file.path}`);
+
+	//data here is the ip address in each file
+	const cleanedText: [] = await response.data.split('\n').filter((line: any) => {
+		return line.includes('#') !== 0;
+	});
+	return cleanedText;
+};
+
+// Determine location of IP Address.
+export const getIPLocationInfo = async (ip: string) => {
+	let info = await geoip.lookup(ip);
+
+	if (info) {
+		return [
+			{
+				region: info.region,
+				country: info.country,
+				timezone: info.timezone
+			}
+		];
+	}
+	return 'no data found';
+};

24fd219a6a8671971d9866be19296b5d3e561126
Ben Halverson <benhalverson33@gmail.com>
2021-05-03 20:52:52 -0700

    refactor: 💡 Added catch statements for axios get requests

--- utils.ts
+++ utils.ts
@@ -6,9 +6,9 @@ import { FireHolFile } from './interfaces';
 export const getData = async () => {
 	const response = await axios.get(
 		`https://api.github.com/repos/firehol/blocklist-ipsets/git/trees/master?recursive=1`
-	);
+	).catch((error: Error) => console.error('Unable to load data from api.github.com: ', error.message));
 
-  //files is each filename in the git repo that ends with .ipset
+	//files is each filename in the git repo that ends with .ipset
 	const files: [] = await response.data.tree.filter((file: any) => {
 		if (file.path.endsWith('.ipset')) {
 			return file.path;
@@ -17,9 +17,10 @@ export const getData = async () => {
 	return files;
 };
 
-
 export const readData = async (file: FireHolFile) => {
-	const response = await axios.get(`https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${file.path}`);
+	const response = await axios
+		.get(`https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/${file.path}`)
+		.catch((error: Error) => console.error('Unable to load data from raw.githubusercontent.com: ', error.message));
 
 	//data here is the ip address in each file
 	const cleanedText: [] = await response.data.split('\n').filter((line: any) => {
@@ -29,7 +30,7 @@ export const readData = async (file: FireHolFile) => {
 };
 
 // Determine location of IP Address.
-export const getIPLocationInfo = async (ip: string) => {
+export const getIPLocationInfo = async (ip: string)=> {
 	let info = await geoip.lookup(ip);
 
 	if (info) {
